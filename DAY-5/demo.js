02d6081d1d119212e6c82835ff4a13a8c86296d8
Adrian Moa <adrian_moa@hotmail.com>
2025-05-16 18:58:50 +0000

    Add functionality to delete. Toogle buttons

--- DAY-5/demo.js
+++ DAY-5/demo.js
@@ -1,7 +1,9 @@
 const url = 'http://localhost:3000/Books';
+const editingMode = false;
 
 async function getAllBooks(){
-    const bookList = (await fetch(url)).json();
+    const promiseRequest = await fetch(url);
+    const bookList = await promiseRequest.json();
     const tableBody = document.getElementById("tableBody");
     
     bookList.forEach(book => {
@@ -9,12 +11,45 @@ async function getAllBooks(){
         <tr>
             <td>${book.id}</td>
             <td>${book.bookTitle}</td>
-            <td>${book.price}</td>
+            <td>${book.price.toLocaleString('en-US', { style:'currency', currency: 'USD'})}</td>
             <td>
-                <button class="btn btn-warning" onclick="editBook('${book.id}')" />
-                <button class="btn btn-danger" onclick="deleteBook('${book.id}')" />
+                <button class="btn btn-warning" onclick="editBook('${book.id}','${book.bookTitle}','${book.price}')">Edit</button>
+                <button class="btn btn-danger" onclick="deleteBook('${book.id}')">Delete</button>
             </td>
         </tr>
         `;
     });
+}
+
+function toogleButtons(editingMode){
+    const submitButton = document.getElementById('submit');
+    const updateButton = document.getElementById('update');
+    const cancelButton = document.getElementById('cancel');
+    if(!editingMode){
+        submitButton.style.display='inline';
+        updateButton.style.display='none';
+        cancelButton.style.display='none';
+    }
+    else{
+        submitButton.style.display='none';
+        updateButton.style.display='inline';
+        cancelButton.style.display='inline';
+    }
+}
+
+function editBook(bookId, bookTitle, price){
+    document.getElementById('bookId').value = bookId;
+    document.getElementById('bookTitle').value = bookTitle;
+    document.getElementById('price').value = price;
+    toogleButtons(true);
+}
+
+async function deleteBook(bookId){
+    const deleteRecord = confirm(`Do you want delete the Book with ID ${bookId}?`);
+    if(!deleteRecord) return;
+
+    await fetch(`${url}/${bookId}`, { method: 'DELETE'});
+    alert('Book deleted succesfully!');
+
+    getAllBooks();
 }

324d763ce2fdcf32df474c18ca47e3b68604b132
Adrian Moa <adrian_moa@hotmail.com>
2025-05-21 18:14:58 +0000

    Lab 15: Form values to state. Add binding to controls and modify save method to update mock values in memory

--- DAY-7 React+Typescript/projectpilot/src/projects/ProjectsPage.tsx
+++ DAY-7 React+Typescript/projectpilot/src/projects/ProjectsPage.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from './ProjectList';
 import { Project } from "./Project";
 
 function ProjectsPage(){
+    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
     const saveProject = (project: Project) => {
-        console.log('Saving project: ', project);
+        let updatedProjects = projects.map((p: Project) => {
+            return p.id === project.id ? project : p;
+        });
+        setProjects(updatedProjects);
     }
 
     return (
@@ -12,7 +17,7 @@ function ProjectsPage(){
             <h1>Projects</h1>
             <ProjectList 
                 onSave={saveProject}
-                projects={MOCK_PROJECTS} />
+                projects={projects} />
         </>
     );
 }

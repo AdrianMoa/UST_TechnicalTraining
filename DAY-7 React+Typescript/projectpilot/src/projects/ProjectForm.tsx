24d3137ce9207d22fdd8f03ca0867a249298f1b2
Adrian Moa <adrian_moa@hotmail.com>
2025-05-21 20:06:11 +0000

    Lab 16-1: Form validations. Add methods to validate form

--- DAY-7 React+Typescript/projectpilot/src/projects/ProjectForm.tsx
+++ DAY-7 React+Typescript/projectpilot/src/projects/ProjectForm.tsx
@@ -10,9 +10,15 @@ interface ProjectFormProps{
 
 function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormProps){
     const [project, setProject] = useState(initialProject);
+    const [errors, setErrors] = useState({
+        name:'',
+        description:'',
+        budget:'',
+    });
 
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
+        if(!isValid()) return;
         onSave(project);
     };
 
@@ -33,7 +39,34 @@ function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormP
             updatedProject = new Project({...p, ...change});
             return updatedProject;
         });
+
+        setErrors(() => validate(updatedProject));
     };
+
+    function validate(project: Project){
+        let errors: any = { name:'', description:'', budget:''};
+        if(project.name.length === 0){
+            errors.name = 'Name is required';
+        }
+        if(project.name.length > 0 && project.name.length < 3){
+            errors.name = 'Name needs to be at least 3 characters.';
+        }
+        if(project.description.length === 0){
+            errors.description = 'Description is required.';
+        }
+        if(project.budget === 0){
+            errors.budget = 'Budget must be more than $0.';
+        }
+        return errors;
+    }
+
+    function isValid(){
+        return (
+            errors.name.length === 0 &&
+            errors.description.length === 0 &&
+            errors.budget.length === 0
+        );
+    }
     
     return (
         <form className="input-group vertical" onSubmit={handleSubmit}>

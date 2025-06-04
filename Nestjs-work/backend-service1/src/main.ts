8bd73aca2d9f02027922ea855fd2875e0e294eb6
Adrian Moa Montores <adrian_moa@hotmail.com>
2025-06-03 18:52:07 -0600

    Add swagger dependencies. Add basic swagger configuration to NestJS Backend API

--- Nestjs-work/backend-service1/src/main.ts
+++ Nestjs-work/backend-service1/src/main.ts
@@ -2,10 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { abortOnError: false, cors: true });
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend NestJS  Documentation')
+    .setDescription('Swagger documentation for NestJS API project')
+    .setVersion('1.0')
+    .addTag('UST Course')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app,  documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

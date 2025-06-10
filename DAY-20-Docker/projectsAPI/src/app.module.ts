c7c0e8acce1e647e03aafa75686522c561b0a877
Adrian Moa Montores <adrian_moa@hotmail.com>
2025-06-10 13:58:30 -0600

    Adjust project to Docker Practice status

--- DAY-20-Docker/projectsAPI/src/app.module.ts
+++ DAY-20-Docker/projectsAPI/src/app.module.ts
@@ -6,10 +6,12 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { UsersModule } from './module/users.module';
 import { AuthModule } from './module/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
         JWT_ACCESS_SECRET: Joi.string().required(),
@@ -22,14 +24,22 @@ import { AuthModule } from './module/auth.module';
         abortEarly: false,
       }
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI ?? '', {dbName: 'projectdb'}), //import for mongodb principal DB
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 10}]),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? '', {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
+
+    //feature modules
     ProjectModule, //Project Module that maps providers and controller related to Project
     UsersModule,
     AuthModule,
   ],
-  //controllers: [AppController],
-  //providers: [AppService],
 })
+
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
--- DAY-20-Docker/projectsAPI/src/main.ts
+++ DAY-20-Docker/projectsAPI/src/main.ts
@@ -1,23 +1,31 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, Version, VersioningType } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { TransformInterceptor } from './common/interceptor/interceptor.transform';
 //import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
 import { configureSwaggerUI } from './swagger.config';
+import helmet from 'helmet';
+import * as compression from 'compression';
+
+const PORT = process.env.PORT ?? 4000;
+const CORS_ORIGIN = process.env.CORS_ORIGIN ?? '*'; //to avoid CORS error on client, specify exact frontend URL
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { abortOnError: false });
   
-  app.enableCors({
-    origin: 'http://localhost:5173', //specific frontend URL
-    credentials: true, //Allow cookies/headers auth
-  });
+  app.enableCors({ origin: CORS_ORIGIN, credentials: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   //app.useGlobalInterceptors(new LoggingInterceptor());
   configureSwaggerUI(app);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableVersioning({ type: VersioningType.URI })
+  app.use(helmet());
+  app.use(compression());
+
+  await app.listen(PORT, () => {
+    console.log(`🚀 Application running at port ${PORT}`)
+  });
 }
 
 bootstrap();
